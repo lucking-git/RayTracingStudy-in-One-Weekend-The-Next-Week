1dddfa6e8793db8027a46636b8716f61bbf15ca5
94500 <94500@PC416>
2023-05-28 01:06:41 +0800

    添加项目文件。

---
+++ raytracing/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "vec3.h"
+
+int main()
+{
+	const int image_width = 200;
+	const int image_height = 100;
+
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = image_height - 1; j >= 0; --j)
+		std::cerr << "\rScanlines remaining:" << j << ' ' << std::flush;
+	{
+		for (int i = 0; i < image_width; ++i)
+		{
+			vec3 color(double(i) / image_width, double(j) / image_height, 0.2);
+			color.write_color(std::cout);
+
+		}
+	}
+	std::cerr << "\nDone.\n";
+
+
+
+
+	return 0;
+//}
